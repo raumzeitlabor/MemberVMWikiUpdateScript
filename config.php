5f63fa6329df536ab9030724ee74eb017eefdf72
Felicitus <felicitus@felicitus.org>
2013-11-19 22:55:15 +0100

    Made script a bit nicer to maintain

---
+++ config.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Defines the total available host memory in MB
+ * @var int
+ */
+$totalHostMemory = 12001;
+
+/**
+ * Defines the total available host storage capacity in GB
+ * @var int
+ */
+$totalHostStorage = 5400;
+
+/**
+ * Defines the API endpoint
+ * @var string
+ */
+$wikiURI = "https://wiki.raumzeitlabor.de/api.php";
+
+/**
+ * Defines the Wiki user
+ * @var string
+ */
+$wikiUsername = "Matebot";
+
+/**
+ * Defines the wiki password
+ * @var string
+ */
+$wikiPassword = "changeme";
+
+/**
+ * Defines the wiki page to edit
+ * @var string
+ */
+$wikiPage = "Mate (Server)";
+
+/**
+ * Defines the wiki section to replace
+ * @var string
+ */
+$wikiSection = "VMs";
+
--- update_wiki_domain_stats.php
+++ update_wiki_domain_stats.php
@@ -2,132 +2,186 @@
 // Script, welches Arbeitsspeicher und HDD-Belegung von libvirt zieht und sie ins RaumZeitLabor-Wiki packt.
 // Fix zusammengehackt von Felicitus am 2013-11-17
 
+require_once 'Zend/Loader.php';
+include("config.php");
 
-$conn = libvirt_connect('null', false);
-$doms = libvirt_list_domain_resources($conn);
+$domainResultSet = getDomains();
 
-$totalMemory = 0;
-$totalStorage = 0;
+$content[] = "== VMs ==";
 
-$content[] = "== VMs ==\n";
+$content[] = '{| class="wikitable"  valign="top"';
+$content[] = "! VM";
+$content[] = "! Arbeitsspeicher";
+$content[] = "! Plattenplatz";
+$content[] = "|-";
 
-$content[] = "{| class=\"wikitable\"  valign = \"top\"\n";
-$content[] = "! VM\n";
-$content[] = "! Arbeitsspeicher\n";
-$content[] = "! Plattenplatz\n";
-$content[] = "|-\n";
+ksort($domainResultSet["domains"]);
 
-foreach ($doms as $dom) {
+$percentMemory = round(($domainResultSet["totalMemory"] / $totalHostMemory) * 100, 2);
+$percentStorage = round(($domainResultSet["totalStorage"] / $totalHostStorage) * 100, 2);
 
-    if (libvirt_domain_get_id($dom) !== -1) {
-        $content[] = "| [[Mate (Server)/" . libvirt_domain_get_name($dom) . "]] \n";
-
-        $memory = round(libvirt_domain_memory_stats($dom)[6] / 1024);
-        $totalMemory += $memory;
-        $content[] = "|" . $memory . " MB \n";
-
-        $storage = 0;
-
-        $devices = libvirt_domain_get_disk_devices($dom);
-        unset($devices["num"]);
-        foreach ($devices as $disk) {
-            $storageInfo = @libvirt_domain_get_block_info($dom, $disk);
-
-            $storage += $storageInfo["capacity"];
-        }
-
-        $storage = round($storage / 1024 / 1024 / 1024, 1);
-        $content[] = "|" . $storage . " GB\n";
-
-        $totalStorage += $storage;
-
-        $content[] = "|-\n";
-    }
+foreach ($domainResultSet["domains"] as $name => $data) {
+    $content[] = "| [[Mate (Server)/" . $name . "]]";
+    $content[] = "| " . $data["memory"] . " MB";
+    $content[] = "| " . $data["storage"] . " GB";
+    $content[] = "|-";
 }
 
-$totalHostMemory = 12001;
-$totalHostStorage = 5400;
+$content[] = "| '''Gesamt'''";
+$content[] = "| '''" . $domainResultSet["totalMemory"] . " MB / " . $totalHostMemory . " MB (" . $percentMemory . "% belegt)'''";
+$content[] = "| '''" . round($domainResultSet["totalStorage"] / 1024, 1) . " TB / " . round(
+        $totalHostStorage / 1024,
+        1
+    ) . " TB (" . $percentStorage . "% belegt)'''";
+$content[] = "|}";
+$content[] = "";
+$content[] = "Automatisch geupdatet von [[Benutzer:Matebot]] am: " . date("Y-m-d H:i:s");
+$content[] = "";
 
-$content[] = "| '''Gesamt'''\n";
-$content[] = "| '''" . $totalMemory . " MB / " . $totalHostMemory . " MB (" . round(
-        ($totalMemory / $totalHostMemory) * 100,
-        2
-    ) . "% belegt)'''\n";
-$content[] = "| '''" . round($totalStorage / 1024, 1) . " TB / " . round($totalHostStorage / 1024, 1) . " TB (" . round(
-        ($totalStorage / $totalHostStorage) * 100,
-        2
-    ) . "% belegt)'''\n";
-$content[] = "|}\n";
+publishToWiki($wikiURI, $wikiUsername, $wikiPassword, $wikiPage, $wikiSection, implode("\n", $content));
 
-$content[] = "\nAutomatisch geupdatet von [[Benutzer:Matebot]] am: " . date("Y-m-d H:i:s") . "\n";
-// load Zend classes
-require_once 'Zend/Loader.php';
-Zend_Loader::loadClass('Zend_Rest_Client');
-Zend_Loader::loadClass('Zend_Http_Client');
-Zend_Loader::loadClass('Zend_Http_CookieJar');
-try {
-    // initialize REST client
-    $client = new Zend_Http_Client();
-    $cookieJar = new Zend_Http_CookieJar();
-    $client->setCookieJar($cookieJar);
-    $wikipedia = new Zend_Rest_Client('https://wiki.raumzeitlabor.de/api.php');
-    $wikipedia->setHttpClient($client);
+/**
+ * Returns an associative array with all running domains, their memory- and storage consumption.
+ *
+ * memory is in MB, storage in GB.
+ * Example return:
+ *
+ * array(
+ *  "totalMemory" => 1024,
+ *  "totalStorage" => 30,
+ *  "domains" => array(
+ *     "infra.rzl" => array("memory" => 1024, "storage" => 30)
+ *   )
+ * )
+ *
+ * @return array
+ */
+function getDomains () {
+    $totalMemory = 0;
+    $totalStorage = 0;
 
-    $wikipedia->action('login');
-    $wikipedia->lgname('Matebot');
-    $wikipedia->lgpassword('hermes88');
-    $wikipedia->format('xml');
-    $result = $wikipedia->post();
+    $conn = libvirt_connect('null', false);
+    $doms = libvirt_list_domain_resources($conn);
 
-    $token = $result->login->attributes()->token[0]->__toString();
-    $wikipedia->action('login');
-    $wikipedia->lgname('Matebot');
-    $wikipedia->lgpassword('hermes88');
-    $wikipedia->format('xml');
-    $wikipedia->lgtoken($token);
-    $result = $wikipedia->post();
+    $domains = array();
 
-    $wikipedia->action('query');
-    $wikipedia->prop('info');
-    $wikipedia->intoken('edit');
-    $wikipedia->titles('RaumZeitLabor:Sandbox');
-    $wikipedia->format('xml');
-    $result = $wikipedia->get();
 
-    $edittoken = $result->query->pages->page->attributes()->edittoken[0]->__toString();
+    foreach ($doms as $dom) {
+        $domain = array();
+        if (libvirt_domain_get_id($dom) !== -1) {
 
-    $wikipedia->action('parse');
-    $wikipedia->page("Mate (Server)");
-    $wikipedia->prop("sections");
-    $wikipedia->format("xml");
+            $content[] = "| [[Mate (Server)/" . libvirt_domain_get_name($dom) . "]] \n";
 
-    $result = $wikipedia->get();
+            $memory = round(libvirt_domain_memory_stats($dom)[6] / 1024);
+            $totalMemory += $memory;
+            $domain["memory"] = $memory;
 
-    $foundSection = false;
-    $sectionCount = 1;
-    foreach ($result->parse->sections->s as $key => $section) {
-        if ($section->attributes()->line == "VMs") {
-            $foundSection = $sectionCount;
-            break;
+            $storage = 0;
+
+            $devices = libvirt_domain_get_disk_devices($dom);
+            unset($devices["num"]);
+            foreach ($devices as $disk) {
+                // The following call triggers a warning for some domains; we'll ignore that as we can't work around
+                $storageInfo = @libvirt_domain_get_block_info($dom, $disk);
+
+                $storage += $storageInfo["capacity"];
+            }
+
+            $storage = round($storage / 1024 / 1024 / 1024, 1);
+            $totalStorage += $storage;
+            $domain["storage"] = $storage;
+
+            $domains[libvirt_domain_get_name($dom)] = $domain;
         }
-        $sectionCount++;
     }
 
-    if ($foundSection === false) {
-        echo "Could not find section named VMs, exit.";
-        exit;
+    return array("domains" => $domains, "totalMemory" => $totalMemory, "totalStorage" => $totalStorage);
+}
+
+/**
+ * Publishes content to a specific wiki page and section.
+ *
+ * @param $apiURI   The API URI endpoint for mediawiki
+ * @param $username The username for the bot
+ * @param $password The password for the bot
+ * @param $page     The page to edit
+ * @param $section  The section name to edit
+ * @param $content  The content to publish
+ */
+function publishToWiki ($apiURI, $username, $password, $page, $section, $content) {
+
+    Zend_Loader::loadClass('Zend_Rest_Client');
+    Zend_Loader::loadClass('Zend_Http_Client');
+    Zend_Loader::loadClass('Zend_Http_CookieJar');
+
+    try {
+        // initialize REST client
+        $client = new Zend_Http_Client();
+        $cookieJar = new Zend_Http_CookieJar();
+        $client->setCookieJar($cookieJar);
+        $wikipedia = new Zend_Rest_Client($apiURI);
+        $wikipedia->setHttpClient($client);
+
+        // Attempt login to get a login token
+        $wikipedia->action('login');
+        $wikipedia->lgname($username);
+        $wikipedia->lgpassword($password);
+        $wikipedia->format('xml');
+        $result = $wikipedia->post();
+
+        $token = $result->login->attributes()->token[0]->__toString();
+
+        // Login with the received token
+        $wikipedia->action('login');
+        $wikipedia->lgname($username);
+        $wikipedia->lgpassword($password);
+        $wikipedia->format('xml');
+        $wikipedia->lgtoken($token);
+        $result = $wikipedia->post();
+
+        // Retrieve an edit token
+        $wikipedia->action('query');
+        $wikipedia->prop('info');
+        $wikipedia->intoken('edit');
+        $wikipedia->titles($page);
+        $wikipedia->format('xml');
+        $result = $wikipedia->get();
+
+        $edittoken = $result->query->pages->page->attributes()->edittoken[0]->__toString();
+
+        // Retrieve the sections for the target page
+        $wikipedia->action('parse');
+        $wikipedia->page($page);
+        $wikipedia->prop("sections");
+        $wikipedia->format("xml");
+
+        $result = $wikipedia->get();
+
+        $foundSection = false;
+        $sectionCount = 1;
+        foreach ($result->parse->sections->s as $key => $oSection) {
+            if ($oSection->attributes()->line == $section) {
+                $foundSection = $sectionCount;
+                break;
+            }
+            $sectionCount++;
+        }
+
+        if ($foundSection === false) {
+            echo "Could not find section named ".$section.", exit.";
+            exit;
+        }
+
+        $wikipedia->action('edit');
+        $wikipedia->title($page);
+        $wikipedia->section($foundSection);
+        $wikipedia->summary('Updated VM list');
+        $wikipedia->text($content);
+        $wikipedia->token($edittoken);
+
+        $result = $wikipedia->post();
+
+    } catch (Exception $e) {
+        print_r($e);
     }
-
-    $wikipedia->action('edit');
-    $wikipedia->title('Mate (Server)');
-    $wikipedia->section($foundSection);
-    $wikipedia->summary('Updated VM list');
-    $wikipedia->text(implode("", $content));
-    $wikipedia->token($edittoken);
-
-    $result = $wikipedia->post();
-
-    print_r($result);
-} catch (Exception $e) {
-    print_r($e);
 }
